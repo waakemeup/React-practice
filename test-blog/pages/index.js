faddbdaf91ccd627a931b7fbb2d62b08f273905f
Ruby <waakemeup@RubydeMacBook-Air.local>
2021-08-17 10:06:25 +0800

    test blog

---
+++ test-blog/pages/index.js
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import fs from 'fs'
+
+const Home = ({slugs}) => {
+  return (
+    <div>
+      slugs:
+      {slugs.map(slug=>{
+        return (
+          <div key={slug}>
+            <Link href={'/blog/'+slug}>
+              <a>{'/blog/'+slug}</a>
+            </Link>
+          </div>)
+      })}
+    </div>
+  )
+}
+
+export const getStaticProps = async () => {
+  const files = fs.readdirSync('posts')
+  return {
+    props:{
+      slugs:files.map(filename=>filename.replace('.md',''))
+    }
+  }
+}
+
+export default Home
