50b518a20708828a7cac83933a539e39fea733af
Ruby <LIU10032@163.com>
2021-10-14 18:44:26 +0800

    随便抄了一段代码

--- tictactoe/src/TicTacToe.jsx
+++ tictactoe/src/TicTacToe.jsx
@@ -1,12 +1,106 @@
-import React from 'react'
+import React, { memo, useState } from 'react'
+import classNames from 'classnames' 
 import './TicTacToe.scss'
 
-const TicTacToe = () => {
+const Square = memo(({value,onClick,index})=>{
   return (
-    <div>
-      
+    <button className="Square" onClick={()=>onClick(index)}>
+      {value}
+    </button>
+  )
+})
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  let winner = lines.reduce((memo,[a,b,c])=>{
+    if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
+      memo = squares[a]
+    }
+    return memo
+  },"")
+  if(!winner && squares.every(s=>s)){
+    winner = 'tie'
+  }
+  return winner
+}
+
+const Board = ({squares,onCLick}) => {
+  return (
+    <div className="Board">
+      {squares.map((square,i)=>(
+        <Square key={i} index={i} value={square} onClick={onCLick}/>
+      ))}
     </div>
   )
 }
 
-export default TicTacToe
+const Announcement = ({winner,onStart}) => {
+  return (
+    <div className="Announcement">
+      {winner === 'tie' ? (
+        <div>Tie Game</div>
+      ):(
+        <div>
+          <div>Congrat</div>
+          <div>{winner}</div>
+        </div>
+      )}
+      <button className="Button" onClick={onStart}>
+        Start
+      </button>
+    </div>
+  )
+}
+
+const Message = ({hasStarted,isXNext}) => {
+  return (
+    <div className="Message">
+      {hasStarted
+        ? isXNext
+          ? "It's Xs turn"
+          : "It's Os turn"
+        : "Click to start game"
+      }
+    </div>
+  )
+}
+
+const Game = () => {
+  const [squares,setSquares] = useState(Array(9).fill(null))
+  const [isXNext,setIsXNext] = useState(true)
+  const [winner,setWinner] = useState("")
+
+  function handleClick(i){
+    const moves = [...squares]
+    moves[i] = isXNext ? 'X':'O'
+    setSquares(moves)
+    setIsXNext(!isXNext)
+    const winner = calculateWinner(moves)
+    if(winner) setWinner(winner)
+  }
+
+  function handleStart(){
+    setSquares(Array(9).fill(null))
+    setWinner(null)
+  }
+
+  return (
+    <div className={classNames("Game",{"Game--winner":!!winner})}>
+      <Board squares={squares} onCLick={handleClick}/>
+      <Message hasStarted={squares.some(s=>s)} isXNext={isXNext}/>
+      {!!winner && <Announcement winner={winner} onStart={handleStart}/>}
+    </div>
+  )
+}
+
+export default Game
